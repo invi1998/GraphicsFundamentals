c785831d01290481283fbbc7141d91e4f46273ff
invi1998 <2267661826@qq.com>
2022-08-21 22:11:42 +0800

    绘制直线代码优化（随意两个点都能处理）

--- GraphicsFundamentals/Raster.cpp
+++ GraphicsFundamentals/Raster.cpp
@@ -5,7 +5,7 @@ namespace CELL {
 		_buffer = (Rgba*)buffer;
 	}
 
-	Raster::~Raster() {}
+	Raster::~Raster() = default;
 
 	void Raster::drawPoint(int x, int y, Rgba color, int ptSize) {
 		switch (ptSize)
@@ -53,11 +53,42 @@ namespace CELL {
 		float xOffset = pt1.x - pt2.x;
 		float yOffset = pt1.y - pt2.y;
 
-		float scope = yOffset / xOffset;
+		float xMin = tmin(pt1.x, pt2.x);
+		float xMax = tmax(pt1.x, pt2.x);
+		float yMin = tmin(pt1.y, pt2.y);
+		float yMax = tmax(pt1.y, pt2.y);
 
-		for (float x = pt1.x; x <= pt2.x; x += 1.0f) {
-			float y = pt1.y + (x - pt1.x) * scope;
-			setPiexl(x, y, color);
+		if (xOffset == 0.0f && yOffset == 0.0f)
+		{
+			setPiexl(xMin, yMin, color);
+		}
+		else if (xOffset == 0.0f)
+		{
+			for (float y = yMin; y <= yMax; y += 1.0f) {
+				setPiexl(xMin, y, color);
+			}
+		}
+		else if (yOffset == 0.0f)
+		{
+			for (float x = xMin; x <= xMax; x += 1.0f) {
+				setPiexl(x, yMax, color);
+			}
+		}
+		else if (fabs(xOffset) > fabs(yOffset)) {
+			float scope = yOffset / xOffset;
+
+			for (float x = xMin; x <= xMax; x += 1.0f) {
+				float y = yMin + (x - xMin) * scope;
+				setPiexl(x, y, color);
+			}
+		}
+		else if (fabs(xOffset) < fabs(yOffset)) {
+			float scope = xOffset / yOffset;
+
+			for (float y = yMin; y <= yMax; y += 1.0f) {
+				float x = xMin + (y - yMin) * scope;
+				setPiexl(x, y, color);
+			}
 		}
 	}
 }
