476d5fc0a6000e567de6ddd3212d23d396236d6e
invi1998 <2267661826@qq.com>
2023-01-16 17:32:00 +0800

    多物体渲染，以及区分不同物体的材质

--- note/shader/3D_SDF_Render.glsl
+++ note/shader/3D_SDF_Render.glsl
@@ -1,5 +1,5 @@
 #define TMIN 0.1
-#define TMAX 20.
+#define TMAX 40.
 #define RAYMARCH_TIME 128   // 最大迭代次数
 #define PRECISION   0.001   // 定义精度
 #define AA 3
@@ -25,37 +25,60 @@ float sdfRect(in vec3 p, in vec3 b) {
     return length(max(d, 0.)) + min(max(d.x, max(d.y, d.z)), 0.);
 }
 
-float map(in vec3 p) {
-    float d = sdfSphere(p);
-    d = min(d, sdfPlane(p + vec3(0., 1., 0.)));
+vec2 opU(vec2 a, vec2 b) {
+    return a.x < b.x ? a : b; 
+}
+
+// 这里返回一个二维向量，第一个值还是他的最短距离，第二个用于标记这个物体（现在要做物体区分）
+vec2 map(in vec3 p) {
+    // float d = sdfSphere(p);
+    // // float d = sdfRect(p, vec3(0.4, 1., 0.3));
+    // d = min(d, sdfPlane(p + vec3(0., 1., 0.)));
+    // return d;
+
+    vec2 d = vec2(sdfSphere(p - vec3(0., 1., 0.)), 2.);    // 球体标记为2
+    // d = opU(d, vec2(sdfPlane(p + vec3(0., 1., 0.)), 1.));       // 平面标记为1
+    d = opU(d, vec2(sdfRect(p - vec3(-2., 1., 1.4), vec3(0.7, 1., 1.)), 3.));
+
+
     return d;
 }
 
 // 射线源头，射线方向
-float rayMarch(in vec3 ro, in vec3 rd) {
+vec2 rayMarch(in vec3 ro, in vec3 rd) {
     // 走的距离t
     float t = TMIN;
+    float tmax = TMAX;
+    vec2 res = vec2(-1.);
+
+    if (rd.y < 0.) {
+        float tp = -ro.y / rd.y;
+        tmax = min(tp, tmax);
+        res = vec2(tp, 1.);
+    }
+
     for (int i = 0; i < RAYMARCH_TIME && t < TMAX; i++) {
         vec3 p = ro + t * rd;
         // float d = sdfSphere(p);
-        float d = map(p);
+        vec2 d = map(p);
         // float d = sdfRect(p, vec3(.4, .4, .5));
-        if (d < PRECISION) {
+        if (d.x < PRECISION) {
+            res = vec2(t, d.y);
             break;
         }
-        t += d;
+        t += d.x;
     }
-    return t;
+    return res;
 }
 
 vec3 calcNormal(in vec3 p) {
     const float h = 0.0001;
     const vec2 k = vec2(1, -1);
     return normalize(
-        k.xyy * map(p + k.xyy*h) +
-        k.yyx * map(p + k.yyx*h) +
-        k.yxy * map(p + k.yxy*h) +
-        k.xxx * map(p + k.xxx*h)
+        k.xyy * map(p + k.xyy*h).x +
+        k.yyx * map(p + k.yyx*h).x +
+        k.yxy * map(p + k.yxy*h).x +
+        k.xxx * map(p + k.xxx*h).x
     );
 }
 
@@ -88,8 +111,10 @@ float softShadow(in vec3 ro, in vec3 rd, float k) {
     // 改良版本
     float res = 1.0;
     float ph = 1e20;
-    for (float t = TMIN; t < TMAX;) {
-        float h = map(ro + rd * t);
+    float tmin = 0.1;
+    float tmax = 10.;
+    for (float t = tmin; t < tmax;) {
+        float h = map(ro + rd * t).x;
         if (h < 0.001) {
             return 0.0;
         }
@@ -103,45 +128,61 @@ float softShadow(in vec3 ro, in vec3 rd, float k) {
 }
 
 vec3 render(vec2 uv) {
-    vec3 color = vec3(0.);
     // 定义摄像机
-    vec3 ro = vec3(0., 1., -2.);
-    // vec3 ro = vec3(2. * cos(iTime), 1., 2. * sin(iTime));
+    // vec3 ro = vec3(0., 3., -6.);
+    vec3 ro = vec3(5. * sin(iTime), 3., -6);
 
     if (iMouse.z > 0.01) {
         float theta = iMouse.x / iResolution.x * 2. * PI;
         float thetay = iMouse.y / iResolution.y *2. *PI;
-        ro = vec3(2. * cos(theta), 2. * sin(thetay), 2. * sin(theta));
+        ro = vec3(6. * cos(theta), 3. * sin(thetay), -6. * sin(theta));
     }
 
     // 目标方向，朝向中心即可
-    vec3 ta = vec3(0.);
+    vec3 ta = vec3(0., 0.5, 0);
     mat3 cam = setCamera(ta, ro, 0.);
 
     // 定义射线方向（从摄像机到屏幕上任意点的方向）,然后归一化（单位向量）
     vec3 rd = normalize(cam * vec3(uv, 1.));
 
-    float t = rayMarch(ro, rd);
+    vec3 bg = vec3(.7, .7, .9);
+    vec3 color = bg - rd.y;
 
-    if (t < TMAX) {
+    vec2 t = rayMarch(ro, rd);
+
+    if (t.y > 0.) {
         // 焦点坐标（在这个例子中就是球体表面坐标
-        vec3 p = ro + t*rd;
+        vec3 p = ro + t.x*rd;
         // 法向量
-        vec3 n = calcNormal(p);
+        vec3 n = (t.y < 1.1) ? vec3(0, 1, 0) : calcNormal(p);
         // 定义光线源
-        vec3 light = vec3(cos(iTime), 2., sin(iTime) - 2.);
-        // vec3 light = vec3(2., 3., 0.);
+        // vec3 light = vec3(2. * cos(iTime), 6., sin(iTime) - 2.);
+        vec3 light = vec3(2., 7., -2.);
         // 计算光线和法向量的夹角余弦（做点乘）
         float dif = clamp(dot(normalize(light - p), n), 0.1, 1.);
 
         // 添加阴影
         p += PRECISION * n;
-        float st = softShadow(p, normalize(light - p), 10.);
-        dif *= st;
+        dif *= softShadow(p, normalize(light - p), 10.);
 
         // 添加环境光
         float amp = 0.5 + 0.3 * dot(n, vec3(0., 0., -1.));
-        color = amp * vec3(1, 0.2, 0.5) + dif * vec3(0.8);
+
+        vec3 c = vec3(0.);
+
+        if (t.y > 2.9 && t.y < 3.1) {
+            // 长方体
+            c = vec3(0.8, 0.16, 1.);
+        } else if (t.y > 1.9 && t.y < 2.1) {
+            // 球体
+            c = vec3(0.2, 0.76, 0.93);
+        } else if (t.y > 0.9 && t.y < 1.1) {
+            // 平面
+            c = vec3(0.31);
+        }
+
+        // color = amp * vec3(1, 0.2, 0.5) + dif * vec3(0.8);
+        color = amp * c + dif * vec3(0.5);
     }
 
     // 伽马校正（对0-1取一个n次幂，以防环境光太低导致模型暗部过暗），这里就取1/2次幂
