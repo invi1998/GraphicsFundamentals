3d4cbb58396511d98095ffb46cd5dcb1b6f35e06
invi1998 <2267661826@qq.com>
2023-02-10 18:59:20 +0800

    rayTracing

---
+++ note/闫令琪-现代计算机图形学基础/RayTracing/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/GraphicsFundamentals/note/闫令琪-现代计算机图形学基础/RayTracing/src/RayTracing.cpp" "CMakeFiles/RayTracing.dir/src/RayTracing.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/src/RayTracing.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ note/闫令琪-现代计算机图形学基础/RayTracing/build/CMakeFiles/RayTracing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracing.dir/src/RayTracing.cpp.obj"
+  "CMakeFiles/RayTracing.dir/src/RayTracing.cpp.obj.d"
+  "RayTracing.exe"
+  "RayTracing.exe.manifest"
+  "RayTracing.pdb"
+  "libRayTracing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
